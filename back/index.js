f008d19a49fdbe7249615d641f33f70d115ff909
ErwanMettouchi <erwanmettouchi@gmail.com>
2025-09-06 22:32:59 +0200

    fix: cors origin added

--- back/index.js
+++ back/index.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 const corsOptions = {
-	origin: ["http://localhost:5174", "http://localhost:5173"],
+	origin: [
+		"http://localhost:5174",
+		"http://localhost:5173",
+		"https://pokedex-chi-ruby.vercel.app",
+	],
 };
 app.use(cors(corsOptions));
 
